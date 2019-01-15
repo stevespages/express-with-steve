73b3893873623d22600589ad82513aeb65c624a2
Steve Greig <greigsteve@gmail.com>
2019-01-15 00:03:33 +0000

    changed directory structure so now sphinx-documentation is inside this the public directory in this repo and being accessed directly by app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/sphinx-documentation/_build/html')));
 
 app.listen(PORT, () => {
 	console.log(`listening on ${PORT}`);
